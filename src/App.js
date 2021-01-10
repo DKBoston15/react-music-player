f270b47eea8fc170e1dd3a8ca1368cb76537d89d
DKBoston15 <46634008+DKBoston15@users.noreply.github.com>
2021-01-10 06:09:48 -0800

    Player State Setup - Player Styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 //Import Styles
 import "./styles/app.scss";
 //Components
@@ -8,9 +8,12 @@ import Player from "./components/Player";
 import data from "./util";
 
 function App() {
+  //State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
